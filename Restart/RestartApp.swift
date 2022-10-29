eb8d8981254a609a7f6f8f3afc80e01e0fe55b9d
sadunenko <a.sadunenko@gmail.com>
2022-10-29 16:04:22 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Tanya on 29.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
